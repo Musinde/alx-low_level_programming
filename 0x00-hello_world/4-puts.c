ea6d814b66f570dbc634fedc3bd620ad06da511a
Musinde <musindej@gmail.com>
2022-06-03 01:48:02 -0700

    Last three of C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - Prints out the exact output displayed.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
